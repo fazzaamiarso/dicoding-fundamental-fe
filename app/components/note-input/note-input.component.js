b096255c77112bd207b11bd40cacbcea8fa8c55e
Fazza Razaq Amiarso <fazzaamiarso@gmail.com>
2024-02-16 01:46:52 +0700

    feat: add validation to form

--- app/components/note-input/note-input.component.js
+++ app/components/note-input/note-input.component.js
@@ -10,23 +10,108 @@ class NoteInput extends HTMLElement {
 
     const clone = document.importNode(template.content, true);
     this.form = clone.querySelector("form");
+    this.titleField = clone.querySelector("#title-input");
+    this.bodyField = clone.querySelector("#body-input");
+
+    this.titleError = clone.querySelector("#title-error");
+    this.bodyError = clone.querySelector("#body-error");
 
     this._shadow = this.attachShadow({ mode: "open" });
     this._shadow.appendChild(clone);
+
+    this.submitHandler = this.submitHandler.bind(this);
+    this.showError = this.showError.bind(this);
+    this.hideError = this.hideError.bind(this);
+  }
+
+  clearFields() {
+    this.titleField.value = "";
+    this.bodyField.value = "";
+  }
+
+  setTitleConstraint(element) {
+    element.setCustomValidity("");
+    if (element.validity.valueMissing) {
+      element.setCustomValidity("Title can't be empty!");
+    }
+  }
+
+  setBodyConstraint(element) {
+    element.setCustomValidity("");
+    if (element.validity.valueMissing) {
+      element.setCustomValidity("Body can't be empty!");
+    }
+  }
+
+  validateTitle(element) {
+    const isValid = element.validity.valid;
+    if (isValid) this.hideError(element);
+    else {
+      this.setTitleConstraint(element);
+      this.showError(element);
+    }
+
+    return isValid;
+  }
+
+  validateBody(element) {
+    const isValid = element.validity.valid;
+    if (isValid) this.hideError(element);
+    else {
+      this.setBodyConstraint(element);
+      this.showError(element);
+    }
+
+    return isValid;
+  }
+
+  showError(element) {
+    const errorEl = element.nextElementSibling;
+    const errorMessage = element.validationMessage;
+
+    if (errorEl && errorMessage) {
+      errorEl.textContent = errorMessage;
+      errorEl.style.display = "block";
+    }
+  }
+
+  hideError(element) {
+    const errorEl = element.nextElementSibling;
+
+    if (!errorEl) return;
+    errorEl.textContent = "";
+    errorEl.style.display = "none";
+  }
+
+  submitHandler(evt) {
+    evt.preventDefault();
+
+    const isTitleValid = this.validateTitle(this.titleField);
+    const isBodyValid = this.validateBody(this.bodyField);
+
+    if (!isTitleValid || !isBodyValid) return;
+
+    this.dispatchEvent(
+      new CustomEvent("add-note", {
+        detail: {
+          title: this.titleField.value,
+          body: this.bodyField.value,
+        },
+      })
+    );
+
+    this.clearFields();
   }
 
   connectedCallback() {
-    this.form.addEventListener("submit", (evt) => {
-      evt.preventDefault();
-      const formData = new FormData(evt.currentTarget);
-
-      this.dispatchEvent(
-        new CustomEvent("add-note", {
-          detail: {
-            title: formData.get("title-input"),
-            body: formData.get("body-input"),
-          },
-        })
+    this.form.addEventListener("submit", this.submitHandler);
+    const validationTriggerEvents = ["blur", "change"];
+    validationTriggerEvents.forEach((eventTrigger) => {
+      this.titleField.addEventListener(eventTrigger, (event) =>
+        this.validateTitle(event.target)
+      );
+      this.bodyField.addEventListener(eventTrigger, (event) =>
+        this.validateBody(event.target)
       );
     });
   }
--- app/components/note-input/note-input.template.js
+++ app/components/note-input/note-input.template.js
@@ -29,14 +29,16 @@ template.innerHTML = `
             padding: .5rem;
         }
     </style>
-    <form>
+    <form novalidate>
         <div class="input-container">
             <label for="title-input">Title</label>
             <input id="title-input" name="title-input" placeholder="please type the title" required  />
+            <p id="title-error" style="color:red; display:none;"></p>
         </div>
         <div class="input-container">
             <label for="body-input">Body</label>
             <textarea id="body-input" name="body-input" placeholder="please type the body" resize="vertical" required></textarea>
+            <p id="body-error" style="color:red; display:none;"></p>
         </div>
         <button>Add note</button>
     </form>
