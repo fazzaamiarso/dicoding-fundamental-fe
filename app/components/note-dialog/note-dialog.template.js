afce5849c5c467f61509295fa63c1e8fcd70aa17
Fazza Razaq Amiarso <fazzaamiarso@gmail.com>
2024-02-29 01:48:03 +0700

    style: add subtle animation to dialog

--- app/components/note-dialog/note-dialog.template.js
+++ app/components/note-dialog/note-dialog.template.js
@@ -4,16 +4,21 @@ template.id = "note-dialog-template";
 template.innerHTML = `
     <style>
       dialog {
+        display: grid;
         outline: none;
         border: none;
+        
         width: min(500px, 90%);
-
         border-radius: 8px;
+
+        transform: scale(1);
+        transition: all .2s ease-out;
       }
 
       dialog:not([open]) {
         pointer-events: none;
         opacity: 0;
+        transform: scale(.9);
       }
 
       html:has(dialog[open][modal-mode="mega"]) {
